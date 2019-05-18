6d3eaed5baf2c4a4f6f73a7fdd0f4c4c2164b6ff
standard-software <standard.software.net@gmail.com>
2019-05-19 08:43:08 +0900

    update copipe_node.js forceMkDirSync

--- scripts/copipe_node.js
+++ scripts/copipe_node.js
@@ -10,6 +10,21 @@ const { existsSync, mkdirSync } = require('fs');
  * dir: './a/b/c/' の形式で指定する
  */
 const forceMkDirSync = (dir) => {
+  const isEnd = (str, search) => {
+    return (str.lastIndexOf(search) === str.length - search.length);
+  };
+
+  const excludeEnd = (str, search) => {
+    if (isEnd(str, search)) {
+      return str.slice(0, -1 * search.length);
+    }
+    return str;
+  };
+
+  // 末尾の[/]を切り取る
+  dir = excludeEnd(dir, '/');
+  // console.log(dir);
+
   dirArray = dir.split('/');
   let path = '';
   dirArray.forEach((dirName) => {
--- source/copipe_core.ts
+++ source/copipe_core.ts
@@ -4,7 +4,7 @@
  */
 
 namespace copipe {
-  export const VERSION = '0.7.4';
+  export const VERSION = '1.0.0 beta';
 }
 
 namespace copipe {
